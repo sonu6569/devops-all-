6449b7c94f7b3406c91995e91b3a33c7e2a9f173
sonu6569 <122419676+sonu6569@users.noreply.github.com>
2023-04-10 13:29:12 +0530

    VPC

--- main.tf
+++ main.tf
@@ -1,21 +1,53 @@
 provider "aws" {
-  region = "ap-south-1"
+  region     = "ap-south-1"
   access_key = "AKIA32NYOY3VSSQ4WAZQ"
   secret_key = "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"
 }
 
-resource "aws_key_pair" "key-tf" {
-  key_name   = "key-tf"
-  public_key = file("${path.module}/id_rsa.pub")
-
-}
-resource "aws_instance" "test" {
-  ami           = "ami-0d81306eddc614a45"
-  instance_type = "t2.micro"
-  key_name  = "${aws_key_pair.key-tf.key_name}"
-
+resource "aws_vpc" "my_vpc" {
+  cidr_block = "172.16.0.0/16"
 
   tags = {
-    Name = "pramod-tf"
+    Name = "OWN_VPC"
   }
+}
+
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.10.0/24"
+  availability_zone = "ap-south-1a"
+
+  tags = {
+    Name = "subnet-1"
+  }
+}
+resource "aws_subnet" "subnet-2" {
+
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = "172.16.11.0/24"
+  availability_zone = "ap-south-1b"
+
+  tags = {
+
+    Name = "subnet-2"
+  }
+}
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "Own-IGW"
+  }
+}
+resource "aws_route_table" "route_pub" {
+ vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+}
+resource "aws_route_table_association" "rta_1" {
+  subnet_id      = aws_subnet.my_subnet.id
+  route_table_id = aws_route_table.route_pub.id
 }
